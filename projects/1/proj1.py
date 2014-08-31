2efef928caf8aa7bc586c34f12add4a59f41f184
pratikmallya <pratik.mallya@gmail.com>
2014-08-31 06:08:12 -0500

    ADD: complete first part of project 1

--- projects/1/proj1.py
+++ projects/1/proj1.py
@@ -20,3 +20,15 @@ EX_GRAPH1 = {0: set([1, 4]),
              7: set([3]),
              8: set([1, 2]),
              9: set([0, 3, 4, 5, 6, 7])}
+
+def make_complete_graph(num_nodes):
+    """return complete graph with n nodes"""
+
+    graph = {}
+
+    for i in range(num_nodes):
+        neighbor_nodes = range(num_nodes)
+        neighbor_nodes.pop(i)
+        graph[i] = set(neighbor_nodes)
+
+    return graph
